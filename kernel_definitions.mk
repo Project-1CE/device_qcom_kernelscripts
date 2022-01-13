AOSPA/android_kernel_build

kernel_definitions: Only use vendor DTB directory if exists.
Change-Id: I8bf4b0ed05524fb315f8576e85ea30f03f40361f

kernel_definitions.mk
@@ -209,4 +209,8 @@ $(RTIC_DTB): $(INSTALLED_KERNEL_TARGET)
 
 # Creating a dtb.img once the kernel is compiled if TARGET_KERNEL_APPEND_DTB is set to be false
 $(INSTALLED_DTBIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(RTIC_DTB)
-	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/**/*.dtb $(RTIC_DTB) > $@
+ifeq (,$(wildcard $(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/vendor))
+	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/qcom/*.dtb $(RTIC_DTB) > $@
+else
+	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/qcom/*.dtb $(RTIC_DTB) > $@
+endif

Project-1CE/device_qcom_kernelscripts

kernel_definitions.mk
@@ -321,4 +321,8 @@ $(RTIC_DTB): $(INSTALLED_KERNEL_TARGET)
 
 # Creating a dtb.img once the kernel is compiled if TARGET_KERNEL_APPEND_DTB is set to be false
 $(INSTALLED_DTBIMAGE_TARGET): $(INSTALLED_KERNEL_TARGET) $(RTIC_DTB)
-	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/**/*.dtb $(RTIC_DTB) > $@
+ifneq (,$(wildcard $(TARGET_KERNEL_SOURCE)/arch/arm64/boot/dts/vendor))
+	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/vendor/qcom/*.dtb $(RTIC_DTB) > $@
+else
+	cat $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/qcom/*.dtb $(RTIC_DTB) > $@
+endif